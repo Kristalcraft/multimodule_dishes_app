bf16710acd233dd31e2a594b132b2dbb886c0fe8
Kristalcraft <dmbezoar@gmail.com>
2023-06-07 17:53:39 +0300

    Initial commit

---
+++ app/src/main/java/com/kristalcraft/justanotherrecipes/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.kristalcraft.justanotherrecipes
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.kristalcraft.justanotherrecipes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
